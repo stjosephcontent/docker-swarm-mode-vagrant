69e1f1e97351ac88b0440cdd12e4b6e31885fa83
Sean Macdonald <sean@crazyhorsecoding.com>
2017-03-06 19:52:15 -0500

    cleaned up cruft and added docker group

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 #	stuff.Todo
 
 - remove gourd-api as a subfolder in git and instead bring it in using git in the provisioner
+
--- provision-base.sh
+++ provision-base.sh
@@ -4,3 +4,7 @@ apt-get update -y
 apt-get install -y python-pip
 pip install --upgrade pip
 pip install --upgrade docker-compose
+groupadd --system docker
+usermod -a -G docker ubuntu
+ln -s /vagrant /home/ubuntu/vagrant
+
